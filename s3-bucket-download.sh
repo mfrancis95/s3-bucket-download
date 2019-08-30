91c4443efc58fac5c8ae0a6535dfe4784e4ecb45
mfrancis95 <mikefrancis95@gmail.com>
2019-08-30 09:50:59 -0400

    Add usage printing

--- s3-bucket-download.sh
+++ s3-bucket-download.sh
@@ -1,4 +1,15 @@
-for line in $(curl -Ls $1 | grep -oP 'y>[^<]+'); do
-    echo "Downloading $1${line:2}..."
-    curl -Ls $1${line:2} > ${line:2}
-done
+show_usage() {
+    echo 'Usage: s3-bucket-download URL'
+    exit $1
+}
+
+if [ $# -eq 0 ]; then
+    show_usage 1
+elif [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    show_usage 0
+else
+    for line in $(curl -Ls $1 | grep -oP 'y>[^<]+'); do
+        echo "Downloading $1${line:2}..."
+        curl -Ls $1${line:2} > ${line:2}
+    done
+fi
